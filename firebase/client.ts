a23e9c2da801a4f80305776f66f0fb8bf90c8198
fsad7 <fsad07@gmail.com>
2025-04-21 23:00:17 -0400

    implement voice generation

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,23 +1,24 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp,getApp, getApps } from "firebase/app";
-import {getAuth} from "firebase/auth";
-import {getFirestore}  from "@firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyCd47sgTVDcQex8GpbiKK2afTIhhzUbDM0",
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    authDomain: "vettai.firebaseapp.com",
+    projectId: "vettai",
     storageBucket: "vettai.firebasestorage.app",
     messagingSenderId: "307131767277",
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    appId: "1:307131767277:web:82c2df8a1f2c4deeedefe4",
     measurementId: "G-8WGL1DRDY3"
 };
 
-// Initialize Firebase
-const app = !getApp.length ? initializeApp(firebaseConfig) : getApp();
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
 
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const db = getFirestore(app)
